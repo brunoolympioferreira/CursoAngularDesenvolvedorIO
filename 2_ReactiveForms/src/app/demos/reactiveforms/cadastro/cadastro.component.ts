ddd7725d54fc22108fbe529a774c92fdc37ffa7c
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-09-20 10:35:37 -0300

    validacoes basicas de formularios

--- 2_ReactiveForms/src/app/demos/reactiveforms/cadastro/cadastro.component.ts
+++ 2_ReactiveForms/src/app/demos/reactiveforms/cadastro/cadastro.component.ts
@@ -1,6 +1,6 @@
 import { Usuario } from './models/usuario';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -11,22 +11,29 @@ export class CadastroComponent implements OnInit {
 
   cadastroForm: FormGroup;
   usuario: Usuario;
+  formResult: string = '';
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.cadastroForm = this.fb.group({
-      nome: [''],
+      nome: ['', Validators.required],
       cpf: [''],
-      email: [''],
+      email: ['', [Validators.required, Validators.email]],
       senha: [''],
       senhaConfirmacao: ['']
     });
   }
 
   adicionarUsuario() {
-    this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
-    console.log(this.usuario)
+    if (this.cadastroForm.dirty && this.cadastroForm.valid) {
+      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
+      this.formResult = JSON.stringify(this.cadastroForm.value);
+      console.log(this.usuario)
+    }
+    else {
+      this.formResult = 'Não submeteu!'
+    }
   }
 
 }
